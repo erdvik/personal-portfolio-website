20df378a14c6ed8519dd8d075809dee556fc84e0
rdv1k <43339320+rdv1k@users.noreply.github.com>
2022-04-20 10:19:56 +0200

    Finish project (kind of)

--- resources/javascript/script.js
+++ resources/javascript/script.js
@@ -17,5 +17,19 @@ darkModeButton.onclick = function() {
 }
 
 
+const timeContainer = document.getElementById("time-container");
+const BIRTH_DAY = "12-10-2017";
+const BIRTH_DAY_DATE = new Date(BIRTH_DAY);
+const intlNumberFormatter = new Intl.NumberFormat("en-US");
+
+setInterval(() => {
+  const now = new Date();
+  const difference = Math.floor(
+    (now.getTime() - BIRTH_DAY_DATE.getTime()) / 1000
+  );
+
+  timeContainer.innerText = intlNumberFormatter.format(difference);
+}, 1000);
+
 
 
